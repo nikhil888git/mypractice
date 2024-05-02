7ffaf32ffd7fd309bf6109db81154cb517355620
HJ03 <102145684+HJ03@users.noreply.github.com>
2024-05-02 12:33:52 +0530

    gm

--- gm.js
+++ gm.js
@@ -3,3 +3,9 @@ console.log('ef');
 console.log('eehr');
 console.log('efew');
 console.log('nikhil');
+
+console.log('jfsj');
+console.log('sfhkj');
+console.log('fsjkd');
+console.log('code to push from local to remote git');
+console.log('1234566');
